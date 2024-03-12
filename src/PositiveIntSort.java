05e013f00bd308c66c1c887c23ed1f42941e8762
xchgeaxeax <xchgeaxeax233@gmail.com>
2024-03-13 07:53:48 +0800

    f**king chaoxing

--- src/PositiveIntSort.java
+++ src/PositiveIntSort.java
@@ -2,21 +2,13 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class PositiveIntSort {
     public static void main(String[] args) {
-//        int[] nums = {2, 4, 3, 1, 5, 6, 7, 8, 9, 10};
         Scanner sc = new Scanner(System.in);
-
-//        System.out.println("Enter the positive numbers of elements in the array: ");
         String input = sc.nextLine();
         sc.close();
-//        String[] nums = input.split(" ");
         int[] nums = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
-//        int count = Integer.parseInt(nums[0]);
         int count = nums[0];
         int[] nums2 = Arrays.copyOfRange(nums, 1, count + 1);
-
         Arrays.sort(nums2);
-
-//        System.out.println("Sorted array in from big to small order: ");
         for (int i = nums2.length - 1; i>=0 ; i--) {
             System.out.print(nums2[i] + " ");
         }
@@ -24,25 +16,3 @@ public class PositiveIntSort {
     }
 
 }
-
-
-
-//import java.util.Scanner;
-//import java.util.Arrays;
-//public class PositiveIntSort {
-//    public static void main(String[] args) {
-////        int[] nums = {2, 4, 3, 1, 5, 6, 7, 8, 9, 10};
-//        Scanner sc = new Scanner(System.in);
-////        System.out.println("Enter the positive numbers of elements in the array: ");
-//        String input = sc.nextLine();
-//        String[] nums = input.split(" ");
-//        String[] nums2 = Arrays.copyOfRange(nums, 1, nums.length);
-//        Arrays.sort(nums2);
-//
-////        System.out.println("Sorted array in from big to small order: ");
-//        for (int i = nums2.length - 1; i>=0 ; i--) {
-//            System.out.print(nums2[i] + " ");
-//        }
-//    }
-//
-//}
--- src/TemperatureConversion.java
+++ src/TemperatureConversion.java
@@ -2,17 +2,12 @@ import java.util.Scanner;
 
 public class TemperatureConversion {
     public static void main(String[] args) {
-//        System.out.println("Fahrenheit to Celsius conversion program");
-//        System.out.println("Please input a Fahrenheit temperature:");
 
-        // Get Fahrenheit temperature from use input, using double
         Scanner scanner = new Scanner(System.in);
         double fahrenheit = scanner.nextDouble();
         scanner.close();
         double celsius = fahrenheitToCelsius(fahrenheit);
 
-        // The output is Fahrenheit temperature rounded to 2
-//        System.out.printf("The Celsius temperature is %.2f\n", celsius);
         System.out.printf("The temperature is %.2f\n", celsius);
     }
 
